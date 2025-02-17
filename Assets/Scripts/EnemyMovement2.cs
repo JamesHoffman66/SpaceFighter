d99772b4831c9b1515e09b627c365af91f1abe5e
JamesHoffman66 <jhoffm12@student.fitchburgstate.edu>
2025-02-17 17:11:31 -0500

    maya assets

--- Assets/Scripts/EnemyMovement2.cs
+++ Assets/Scripts/EnemyMovement2.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour
+public class EnemyMovement2 : MonoBehaviour
 {
     //variable defining the movement speed for the enemy
-    private float speed = 25f;
+    private float spd = 25f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         //enemy moves in the direction of the player character
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.back * Time.deltaTime * spd);
     }
 }
